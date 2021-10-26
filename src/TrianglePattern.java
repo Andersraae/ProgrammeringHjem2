7c7130373e18e291898d4d418ff040e0433cb5cc
Bonato99 <simbonato@gmail.com>
2021-10-26 19:49:22 +0200

    værsgo

---
+++ src/TrianglePattern.java
@@ -0,0 +1,24 @@
+public class TrianglePattern {
+    int height;
+    int width;
+    int[] initial;
+
+    public TrianglePattern(int n, int h, int[] initial) {
+        this.height = h;
+        this.width = n;
+        this.initial = initial;
+
+    }
+
+    public static void main(String[] args){
+        new TrianglePattern(50, 2, 20);
+
+    }
+    public int getValueAt(int r, int c){
+
+
+
+
+
+    }
+}
