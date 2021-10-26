85fa4cf096cd0c86cf0dd8c821ddbd07f5eab0ee
Andersraae <andersraae01@gmail.com>
2021-10-26 19:46:49 +0200

    opg1 done

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TextAnalysis {
@@ -15,6 +14,8 @@ public class TextAnalysis {
     private String lastWord = "";
     private int numOfRep = 0;
 
+    private ArrayList<String> words = new ArrayList<>();
+
     public TextAnalysis(String sourceFileName, int maxNoOfWords){
         this.fileName = sourceFileName;
         this.maxWords = maxNoOfWords;
@@ -24,7 +25,7 @@ public class TextAnalysis {
     //Private methods
 
     public static void main(String[] a){
-        TextAnalysis ta = new TextAnalysis("text17_00.txt", 50);
+        TextAnalysis ta = new TextAnalysis("hamlet.txt", 50000);
         System.out.println("word count = " + ta.wordCount());
         System.out.println("different words = " + ta.getNoOfDifferentWords());
         System.out.println("repetitions = " + ta.getNoOfRepetitions());
@@ -34,28 +35,34 @@ public class TextAnalysis {
         try {
             File file = new File(this.fileName);
             Scanner scandi = new Scanner(file); // Opretter en scanner "scandi" som scammer filen
+
             while(scandi.hasNextLine()){
                 if (this.numOfWords == this.maxWords){
                     break;
                 }
                 String line = scandi.nextLine();
                 String[] tokens = line.split("[^a-zA-Z]+");
+                ArrayList<String> tokenArray = new ArrayList<>();
+                for (int a = 0; a < tokens.length; a++){
+                    if (!tokens[a].equals("")){
+                        tokenArray.add(tokens[a]);
+                    }
+                }
 
                 //Number of words, repetitions and unique words
-                int newWords = this.numOfWords + tokens.length;
+                int newWords = this.numOfWords + tokenArray.size();
                 int loopcount;
                 if (newWords >= this.maxWords) {
                     loopcount = this.maxWords - newWords;
                 } else {
-                    loopcount = tokens.length;
+                    loopcount = tokenArray.size();
                 }
 
 
-                ArrayList<String> words = new ArrayList<>();
                 for (int i = 0; i < loopcount; i++) {
-                    String theWord = tokens[i].toLowerCase();
-                    if (!words.contains(theWord)){
-                        words.add(theWord);
+                    String theWord = tokenArray.get(i).toLowerCase();
+                    if (!this.words.contains(theWord)){
+                        this.words.add(theWord);
                         this.numOfUnique++;
                     }
                     if (this.lastWord.equals(theWord)) {
