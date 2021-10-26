74dd02971d9fcb8d916cc5e31f41c86d6fcee06a
Andersraae <andersraae01@gmail.com>
2021-10-26 20:22:57 +0200

    noget med opgave 2

--- src/TrianglePattern.java
+++ src/TrianglePattern.java
@@ -1,24 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TrianglePattern {
     int height;
     int width;
-    int[] initial;
+    ArrayList<Integer> initial;
 
     public TrianglePattern(int n, int h, int[] initial) {
         this.height = h;
         this.width = n;
-        this.initial = initial;
+        this.initial = new ArrayList<Integer>(initial.length);
+        for (int i : initial)
+        {
+            this.initial.add(i);
+        }
+        createGrid();
+    }
+
+    public static void main(String[] a){
+        int[] init = { 10 };
+        int n = 21, h = 8;
+        TrianglePattern tp = new TrianglePattern(n, h, init);
+        System.out.println(n);
+        System.out.println(h);
+        System.out.println(Arrays.toString(init));
+    }
+
+    public void createGrid(){
+        ArrayList<Integer> initialRow = new ArrayList<>(this.width);
+        for (int i = 0; i < this.width; i++){
+            if (this.initial.contains(i)){
+                initialRow.add(1);
+            } else {
+                initialRow.add(0);
+            }
+            System.out.print(initialRow.get(i));
+        }
+
+        //https://stackoverflow.com/questions/2432866/add-objects-with-different-name-through-for-loop/2432885
+
 
     }
 
-    public static void main(String[] args){
-        new TrianglePattern(50, 2, 20);
-
-    }
     public int getValueAt(int r, int c){
-
-
-
-
-
+        return 0;
     }
 }
